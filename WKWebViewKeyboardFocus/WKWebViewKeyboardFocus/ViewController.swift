55c31d23f98dcfe14d274c881e958f00bf4c122b
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2022-09-01 11:31:45 +0200

    chore: Initial storyboard-free skeleton of the WKWebViewKeyboardFocus demo app
    It doesn't do anything yet, it's just the starting point for the demo app.

---
+++ WKWebViewKeyboardFocus/WKWebViewKeyboardFocus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WKWebViewKeyboardFocus
+//
+//  Created by Johannes Fahrenkrug on 9/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
