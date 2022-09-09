1ff1dc044fef43e5a1b2473e0395561ada510a9a
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2022-09-09 08:40:05 +0200

    feat: Add blank new project for PushNotificationsTest demo

---
+++ PushNotificationsTest/PushNotificationsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PushNotificationsTest
+//
+//  Created by Johannes Fahrenkrug on 9/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
